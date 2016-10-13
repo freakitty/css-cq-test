90ee8d158242a2a8aeed2239777bd13eb4a53243
Cindy Castano <castano.cindy1@gmail.com>
2016-10-13 10:11:46 +0200

    Update listitem.js

--- src/client/components/listitem.js
+++ src/client/components/listitem.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const ListItem = ({ name, value }) => (console.log(value),
-  <li>
+const ListItem = ({ name, value, idx }) => (console.log(value),
+  <li className={idx%5?'income':'outcome'}>
     <span>{ `${name} :`}</span>
-    <span>{ `[${value}]`}</span>
+    <span className="value">{ `${value}`}</span>
   </li>)
 
 ListItem.propTypes = {
